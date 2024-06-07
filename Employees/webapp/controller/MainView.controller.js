db8fe961c435d0e38ab8d014a91a9f7b909484a0
Seidordrv <dveron@seidor.es>
2024-06-07 15:53:32 +0000

    First Commit 04

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,12 +1,27 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("logaligroup.employees.controller.MainView", {
-        onInit: function () {
 
-        }
+
+        var Main = Controller.extend("logaligroup.employees.controller.MainView", {});
+
+            Main.prototype.onValidate = function () {
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length === 6) {
+                    //inputEmployee.setDescription("OK");
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("slCountry").setVisible(true);
+                } else {
+                    //inputEmployee.setDescription("Not OK");
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("slCountry").setVisible(false);
+                };
+            };
+
+        return Main;
     });
-});

81bc4241f95909588f769a4c51e985de24e7639e
Seidordrv <dveron@seidor.es>
2024-08-01 15:15:55 +0000

    17 Controladores Personalizados

--- webapp/controller/OrderDetails.controller.js
+++ webapp/controller/OrderDetails.controller.js
@@ -1,33 +1,39 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
-  ], function (Controller, History) {
-  
-    function _onObjectMatched(oEvent){
-      this.getView().bindElement({
-           path: "/Orders(" + oEvent.getParameter("arguments").OrderID + ")",
-           model: "odataNorthwind" 
-      });
-    }
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/core/routing/History"
+], function (Controller, History) {
 
-    return Controller.extend("logaligroup.Employees.controller.OrderDetails", {
-  
-        onInit: function () {
-          var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-          oRouter.getRoute("RouteOrderDetails").attachPatternMatched(_onObjectMatched, this);
-          
-        },
-
-        onBack : function(oEvent) {
-          var oHistory = History.getInstance();
-          var sPreviousHash = oHistory.getPreviousHash();
-
-          if(sPreviousHash !== undefined){
-            window.history.go(-1);
-          } else {
-            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("RouteMain", true);  
-          }
-        }      
+  function _onObjectMatched(oEvent) {
+    this.getView().bindElement({
+      path: "/Orders(" + oEvent.getParameter("arguments").OrderID + ")",
+      model: "odataNorthwind"
     });
-  });      
+  }
+
+  return Controller.extend("logaligroup.Employees.controller.OrderDetails", {
+
+    onInit: function () {
+      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+      oRouter.getRoute("RouteOrderDetails").attachPatternMatched(_onObjectMatched, this);
+
+    },
+
+    onBack: function (oEvent) {
+      var oHistory = History.getInstance();
+      var sPreviousHash = oHistory.getPreviousHash();
+
+      if (sPreviousHash !== undefined) {
+        window.history.go(-1);
+      } else {
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo("RouteMain", true);
+      }
+    },
+
+    onClearSignature: function (oEvent) {
+      var signature = this.byId("signature");
+      signature.clear();
+
+    }
+  });
+});      
